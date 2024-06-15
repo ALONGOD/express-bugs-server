5ef49692b1981d3266a0d6860dcd0f68f672d181
ALONGOD <alongoddard1@gmail.com>
2024-06-15 12:11:32 +0300

    Finish frontend

--- public-simple/js/cars.js
+++
@@ -1,8 +0,0 @@
-function loadCars() {
-    fetch('/api/car')
-        .then(res => res.json())
-        .then(cars => {
-            const elPre = document.querySelector('pre')
-            elPre.innerHTML = JSON.stringify(cars, null, 2)
-        })
-}
